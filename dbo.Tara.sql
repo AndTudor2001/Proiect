4b6b6ca52a6a63647994f434a696838f7e640024
marachereches <marachereches@yahoo.com>
2022-12-14 20:30:16 +0200

    tabelele fara chei straine

--- dbo.Tara.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Tara] (
-    [ID]        INT            IDENTITY (1, 1) NOT NULL,
-    [Nume]      NVARCHAR (MAX) NOT NULL,
-    [Continent] NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_Tara] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
